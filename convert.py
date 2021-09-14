b64755eee94701cb31582b23009682943b2f12dd
blackstone_99 <imbistone@gmail.com>
2021-09-14 10:24:19 +0800

    增加文件下载函数

--- convert.py
+++ convert.py
@@ -32,6 +32,8 @@ def data_convert(template, data, tc):
     tc        对应关系
   """
   for dst, src in tc.items():
+    # 处理表格字段
+    # 两个平台上的表格都是一个列表
     if '(' in dst:
       values = []
       src_groups = re.match(r'(.*?)\.\((.*?)\)', src)
@@ -46,7 +48,12 @@ def data_convert(template, data, tc):
         values.append(t)
       # 获取要更新的字段
       template = parse(dst_groups.group(1)).update(template, values)
-      
+
+    # 处理文件字段
+    # 国家平台上的文件是一个列表
+    elif dst == 'files':
+      pass
+
     else:
       matchs = parse(src).find(data)
       if len(matchs) == 1:
@@ -54,6 +61,33 @@ def data_convert(template, data, tc):
         template = parse(dst).update(template, matchs[0].value)
     
   return template
+
+def download_file(url, path='files', filename=''):
+  # 下载文件，并返回文件保存路径
+  r = requests.get(url).content
+  filename = get_file_name(url, r.headers)
+  with open(os.path.join(path, filename), 'wb+') as f:
+    f.write(r)
+  return os.path.join(path, filename)
+
+def get_file_name(url, headers):
+  # 获取文件名
+  # https://blog.csdn.net/mbh12333/article/details/103721834
+  filename = ''
+  if 'Content-Disposition' in headers and headers['Content-Disposition']:
+    disposition_split = headers['Content-Disposition'].split(';')
+    if len(disposition_split) > 1:
+      if disposition_split[1].strip().lower().startswith('filename='):
+        file_name = disposition_split[1].split('=')
+        if len(file_name) > 1:
+          filename = unquote(file_name[1])
+  elif not filename and os.path.basename(url):
+      filename = os.path.basename(url).split("?")[0]
+  elif filename == '' and data_type != '':
+    filename = str(uuid.uuid4())
+  else:
+    raise Exception('文件名未指定')
+  return filename
 
 if __name__ == '__main__':
   with open('json_files/性能数据.json', 'r', encoding='UTF-8') as f:
